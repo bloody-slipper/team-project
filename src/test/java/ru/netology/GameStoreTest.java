0afecfe34fb32f8acd5457304a8b5326fda01854
bloody-slipper <anniershova@gmail.com>
2022-08-27 19:00:14 +0300

    added tests to GameStoreTest

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class GameStoreTest {
+    GameStore store = new GameStore();
 
     @Test
     public void shouldAddGame() {
@@ -14,5 +15,44 @@ public class GameStoreTest {
         assertTrue(store.containsGame(game));
     }
 
-    // другие ваши тесты
+    @Test
+    public void shouldContainsGameIfOneInList() {
+
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        assertTrue(store.containsGame(game1));
+    }
+    @Test
+    public void shouldContainsTwoGame() {
+
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Как достать соседа", "Аркады");
+
+        assertTrue(store.containsGame(game1));
+    }
+
+    @Test
+    public void shouldGetSumPlayedTime() {
+
+        store.addPlayTime("Irina47", 4);
+        store.addPlayTime("Kolya55", 7);
+
+        int expected = 11;
+        int actual = store.getSumPlayedTime();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAddPlayTime() {
+        store.addPlayTime("Irina47", 4);
+        store.addPlayTime("Irina47", 1);
+
+        int expected = 5;
+        int actual = store.getSumPlayedTime();
+
+        assertEquals(expected, actual);
+
+    }
+
 }
